c5b97e4c2ce07b082ebf677524c2b754cc230e96
Mayo <arnau.mayoral10@gmail.com>
2023-10-16 10:36:33 +0200

    name change

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='EthicalGatheringGame',
+setup(name='MultiAgentEthicalGatheringGame',
       version='0.1',
       install_requires=['gym', 'matplotlib', 'numpy']
       )
