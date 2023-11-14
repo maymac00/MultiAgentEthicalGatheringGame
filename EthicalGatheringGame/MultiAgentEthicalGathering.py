322f598d4fc7f6db39e0121efd5c70bcc071b8b3
arnau <arnau.mayoral10@gmail.com>
2023-11-14 14:56:37 +0100

    done is now vectorial

--- EthicalGatheringGame/MultiAgentEthicalGathering.py
+++ EthicalGatheringGame/MultiAgentEthicalGathering.py
@@ -248,7 +248,7 @@ class MAEGG(gym.Env):
         if self.track:
             self.history.append(info)
 
-        return nObservations, np.array([ag.r for ag in self.agents.values()]), done, info
+        return nObservations, np.array([ag.r for ag in self.agents.values()]), [done] * self.n_agents, info
 
     def reset(self, seed=None):
         """
