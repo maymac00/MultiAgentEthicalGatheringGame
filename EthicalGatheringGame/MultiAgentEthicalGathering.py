ee570f936de38607ad108d72b90ac04166920953
arnau <arnau.mayoral10@gmail.com>
2024-04-09 11:17:45 +0200

    bug fixed

--- EthicalGatheringGame/MultiAgentEthicalGathering.py
+++ EthicalGatheringGame/MultiAgentEthicalGathering.py
@@ -670,7 +670,7 @@ class MAEGG(gym.Env):
             return all_tags, counter
 
         if type == "apple_history":
-            if not (len(self.history) == 0 or len(self.stash) == 0):
+            if len(self.history) == 0 and len(self.stash) == 0:
                 raise ValueError("No history to retrieve")
             if len(self.history) > 0:
                 self.stash.append(self.build_history_array())
