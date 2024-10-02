dcdf889872aed582518370c0144816470c0c35fd
maymac00 <arnau.mayoral10@gmail.com>
2024-10-02 12:10:05 +0200

    proper gym registration of the presets

--- EthicalGatheringGame/__init__.py
+++ EthicalGatheringGame/__init__.py
@@ -39,13 +39,12 @@ register(
 for db in [0, 1, 10, 1000]:
     for we in [0, 10, 2.6]:
         for eff_rate in [0, 0.2, 0.4, 0.6, 0.8, 1]:
-            p = {**presets.large}
-            p = dict(p)
+            p = dict(**presets.large)
             p["donation_capacity"] = db
             p["we"] = [1, we]
             p["efficiency"] = [0.85] * int(5 * eff_rate) + [0.2] * int(5 - eff_rate *5)
             register(
                 id=f'MultiAgentEthicalGathering-large-db{db}-eff{eff_rate}-we{we}-v1',
                 entry_point='EthicalGatheringGame.MultiAgentEthicalGathering:MAEGG',
-                kwargs= {**presets.large, "donation_capacity": db, "we": [1, we], "efficiency": [eff_rate]*5}
+                kwargs= {**p}
             )
